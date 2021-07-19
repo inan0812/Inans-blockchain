f5bc44e7c80fb81b91f74f83aa22c5cd3a32d7eb
inan0812 <dannymunoz07@gmail.com>
2021-07-18 22:43:39 -0400

    Test DNS

--- inan/cmds/configure.py
+++ inan/cmds/configure.py
@@ -111,7 +111,7 @@ def configure(
         elif testnet == "false" or testnet == "f":
             print("Setting Mainnet")
             mainnet_port = "34448"
-            mainnet_introducer = "introducer.chia.net"
+            mainnet_introducer = "introducer.scampool.org"
             net = "mainnet"
             config["full_node"]["port"] = int(mainnet_port)
             config["full_node"]["introducer_peer"]["port"] = int(mainnet_port)
--- inan/full_node/full_node.py
+++ inan/full_node/full_node.py
@@ -187,7 +187,7 @@ class FullNode:
             dns_servers = self.config["dns_servers"]
         elif self.config["port"] == 34448:
             # If `dns_servers` misses from the `config`, hardcode it if we're running mainnet.
-            dns_servers.append("dns-introducer.chia.net")
+            dns_servers.append("dns-introducer.scampool.org")
         try:
             self.full_node_peers = FullNodePeers(
                 self.server,
